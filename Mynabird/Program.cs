bce49d12953f9261ad1a3c143436e7b146e5e155
Jan Grzymala-Busse <jbusse@cboe.com>
2020-11-03 10:31:12 -0600

    Initial add

--- Mynabird/Program.cs
+++ Mynabird/Program.cs
@@ -10,6 +10,10 @@ namespace Mynabird
     {
         static void Main(string[] args)
         {
+            foreach (string arg in args)
+            {
+                System.Console.WriteLine(arg);
+            }
         }
     }
 }
